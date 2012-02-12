c3ef805fbdc85b2fed50f5fc4fa49e650493f689
Pär Winzell <zell@threerings.net>
2012-02-12 00:20:18 -0600

    Add the 'list tables' command.

---
+++ src/main/java/com/alyx/jynamo/handlers/ListTables.java
@@ -0,0 +1,37 @@
+//
+// Who - Copyright 2010-2011 Three Rings Design, Inc.
+
+package com.alyx.jynamo.handlers;
+
+import com.alyx.jynamo.CmdHandler;
+import com.alyx.jynamo.JynParser;
+import com.amazonaws.services.dynamodb.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodb.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodb.model.DescribeTableResult;
+import com.amazonaws.services.dynamodb.model.ListTablesRequest;
+import com.amazonaws.services.dynamodb.model.ListTablesResult;
+import joptsimple.OptionSpec;
+
+import static com.alyx.Log.log;
+
+public class ListTables extends CmdHandler {
+    public ListTables (AmazonDynamoDBClient client) {
+        super(client);
+    }
+
+    @Override
+    public String handlesCommand () {
+        return "list";
+    }
+
+    @Override
+    public void execute (JynParser parser, String[] input, OptionSpec<String> table, boolean justTypes) {
+        if (justTypes) {
+            return;
+        }
+
+        ListTablesResult result = _client.listTables();
+
+        System.out.println("LIST TABLES: " + result);
+    }
+}
